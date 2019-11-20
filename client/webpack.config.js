a85ea669936262ac90efafdb5d778e055f2bd07d
Diego Leonardo Urban <urban@dynamix.com.br>
2019-11-20 10:30:41 -0300

    dynamic service url

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -36,7 +36,11 @@ plugins.push(
     })
 );
 
+let SERVICE_URL = JSON.stringify('http://localhost:3000');
+
 if(process.env.NODE_ENV == 'production') {
+
+    SERVICE_URL = JSON.stringify('http://endereco-da-api');
 
     plugins.push(new webpack.optimize.ModuleConcatenationPlugin());
     plugins.push(new babiliPlugin());
@@ -51,6 +55,10 @@ if(process.env.NODE_ENV == 'production') {
         canPrint: true
      }));
 }
+
+plugins.push(new webpack.DefinePlugin({
+    SERVICE_URL
+}));
 
 module.exports = {
     entry: {
